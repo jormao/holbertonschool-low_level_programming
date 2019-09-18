31a621e4a338e115f76880c937d64524ce7ed7e8
Jorge Salazar <jormao@gmail.com>
2019-09-18 22:25:52 +0000

    test name 5.0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - texto
+ *
+ * Return: text in printf
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
