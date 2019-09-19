3c80e5f5d86b1cbb357cb6dd34c41b549d879f9b
Jorge Salazar <jormao@gmail.com>
2019-09-19 00:12:30 +0000

    size test 6.1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 /**
- * prints the size of various types
+ * main - prints the size of various types
  *
  * Return: cero
  */
 int main(void)
 {
-	printf("Size of a char: %d", sizeof(char));
-	printf("Size of an int: %d", sizeof(int));
-	printf("Size of a long int: %d", sizeof(long int));
-	printf("Size of a long long int: %n", sizeof(long long int));
-	printf("Size of a float: %d", sizeof(float));
+	char chartype;
+	int inttype;
+	long int longinttype;
+	long long int l_l_inttype;
+	float floattype;
+	printf("Size of a char: %d", sizeof(chartype));
+	printf("Size of an int: %d", sizeof(inttype));
+	printf("Size of a long int: %d", sizeof(longinttype));
+	printf("Size of a long long int: %n", sizeof(l_l_inttype));
+	printf("Size of a float: %d\n", sizeof(floattype));
 	return (0);
 }
