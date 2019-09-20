69befefc7e1c4d04f47ea82f62ab6072bc359ec7
Jorge Salazar <jormao@gmail.com>
2019-09-20 04:19:59 +0000

    The success combination in business v12.0

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: cero
+ */
+int main(void)
+{
+	int i = 49;
+	int j = 48;
+	int k = 50;
+	int a = 44;
+	int b = 32;
+	int c = 49;
+	int d = 50;
+	int e = 50;
+	/*solo se puede usar 6 putchar*/
+	for (; j < 56; j++)
+	{
+		for (; i <= 57; i++)
+		{
+			for (; k <= 57; k++)
+			{
+				putchar(j);
+				putchar(i);
+				putchar(k);
+				if (((j == 55) && (i == 56)) && (k == 57))
+					break;
+				putchar(a);
+				putchar(b);
+			}
+			d++;
+			k = d;
+		}
+		e++;
+		k = e;
+		c++;
+		i = c;
+	}
+	putchar('\n');
+	return (0);
+}
