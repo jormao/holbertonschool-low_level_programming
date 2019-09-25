45a19f56a94d64cc992d7b0970e6893af8b3e475
Jorge Salazar <jormao@gmail.com>
2019-09-25 04:12:39 +0000

    Nature made the natural numbers v13.5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 /**
  * main - prints the sum of all the multiples of 3 or 5 below 1024
@@ -9,13 +8,13 @@ int main(void)
 {
 /*this function prints the sum multiples 3 and 5 below 1024.*/
 
-	int i, j, suma;
+	int i, suma;
 
 	suma = 0;
 	for (i = 1; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		    suma = suma + i;
+	if (i % 3 == 0 || i % 5 == 0)
+		suma = suma + i;
 	}
 	printf("%d\n", suma);
 	return (0);
