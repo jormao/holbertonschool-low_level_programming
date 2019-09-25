eee3def4396b1797a0d56747bd7805df0e1de66c
Jorge Salazar <jormao@gmail.com>
2019-09-25 04:57:57 +0000

    fibonacci v15.0

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: 0
+*/
+int main(void)
+{
+/*this function prints prints the first 50 Fibonacci numbers.*/
+
+	int j;
+	long int fi1, fi2, fi3, suma;
+
+	fi1 = 1;
+	fi2 = 0;
+	for (j = 1; j < 50; j++)
+	{
+		fi3 = fi1 + fi2;
+		if (fi3 <= 4000000)
+			suma = suma + fi3;
+		fi2 = fi1;
+		fi1 = fi3;
+	}
+	printf("%ld\n", suma);
+	return (0);
+}
