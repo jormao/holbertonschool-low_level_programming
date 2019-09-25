d8028291105b6a5ea3bd5b40ed3714b99f8d84d0
Jorge Salazar <jormao@gmail.com>
2019-09-25 04:51:36 +0000

    fibonacci v14.1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,19 @@ int main(void)
 {
 /*this function prints prints the first 50 Fibonacci numbers.*/
 
-	int j, fi1, fi2, fi3;
+	int j;
+	long int fi1, fi2, fi3;
 
 	fi1 = 1;
 	fi2 = 0;
 	for (j = 1; j < 50; j++)
 	{
 		fi3 = fi1 + fi2;
-		printf("%d, ", fi3);
+		printf("%ld, ", fi3);
 		fi2 = fi1;
 		fi1 = fi3;
 	}
 	fi3 = fi1 + fi2;
-	printf("%d\n", fi3);
+	printf("%ld\n", fi3);
 	return (0);
 }
