f113eae386d3bb8d43fa074880722e9e15b1ea9a
Jorge Salazar <jormao@gmail.com>
2019-09-26 18:41:02 +0000

    am diagonally parked v.0.0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ *@n: n times to print _
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, a, b;
+
+	/*draws a diagonal line on the terminal with / */
+
+	if (n > 0)
+	{
+		_putchar('\\');
+		_putchar('\n');
+		a = n;
+		b = n;
+		for (i = 2; i <= n; i++)
+		{
+			for (; a <= n; a++)
+			{
+			_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			b--;
+			a = b;
+		}
+	}
+	else
+		_putchar('\n');
+}
