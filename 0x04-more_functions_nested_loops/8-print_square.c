7d84b57c6f988fa7f381321c274fb2cdb8dd43b8
Jorge Salazar <jormao@gmail.com>
2019-09-26 19:12:28 +0000

    so much sunshine v0.0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square.
+ *@size: size of the square
+ *
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	/*prints a square define by size */
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+			_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
