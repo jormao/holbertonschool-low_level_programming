c953a5b0ac5b1359e5b285fa7a5ca725ad83ac2e
Jorge Salazar <jormao@gmail.com>
2019-09-26 02:12:46 +0000

    prints the first 98 Fibonacci numbers v16.9

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,11 +10,13 @@ int main(void)
 {
 
 	int j;
-	uint64_t fi1 = 1, fi2 = 0, fi3;
+	uint64_t fi1 = 1, fi2 = 0, z;
+	__int128 fi3;
 
 	for (j = 1; j < 98; j++)
 	{
 		fi3 = fi1 + fi2;
+		z = fi3 % 10;
 		printf("%." PRIu64 ", ", fi3);
 		fi2 = fi1;
 		fi1 = fi3;
