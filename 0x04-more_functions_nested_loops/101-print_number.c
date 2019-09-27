d5093d7e21115dfe151263a2fa695a9dc00c943d
Jorge Salazar <jormao@gmail.com>
2019-09-27 02:27:11 +0000

    Numbers have life v.12.5

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,6 @@
 
 void print_number(int n)
 {
-	int dm, um, c, d, u, cm;
 
 	/*prints an integer only with putchar */
 
@@ -17,22 +16,7 @@ void print_number(int n)
 		_putchar('-');
 		n = n * -1;
 	}
-	cm = n / 100000;
-	dm = (n - (cm * 100000)) / 10000;
-	um = (n - (cm * 100000) - (dm * 10000)) / 1000;
-	c = (n - (cm * 100000) - (dm * 10000) - (um * 1000)) / 100;
-	d = (n - (cm * 100000) - (dm * 10000) - (um * 1000) - (c * 100)) / 10;
-	u = n % 10;
-
-	if (cm > 0)
-		_putchar('0' + cm);
-	if (cm > 0 || dm > 0)
-		_putchar('0' + dm);
-	if (cm > 0 || dm > 0 || um > 0)
-		_putchar('0' + um);
-	if (cm > 0 || dm > 0 || um > 0 || c > 0)
-		_putchar(c + '0');
-	if (cm > 0 || dm > 0 || um > 0 || c > 0 || d > 0)
-		_putchar(d + '0');
-	_putchar(u + '0');
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n%10 + '0');
 }
