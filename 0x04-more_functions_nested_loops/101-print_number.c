e06fe13c064116fac07583e5d36fd10c1e15889a
Jorge Salazar <jormao@gmail.com>
2019-09-27 02:03:57 +0000

    Numbers have life v.12.4

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,7 @@
 
 void print_number(int n)
 {
-	int dm, um, c, d, u, cm, UM;
+	int dm, um, c, d, u, cm;
 
 	/*prints an integer only with putchar */
 
@@ -17,25 +17,22 @@ void print_number(int n)
 		_putchar('-');
 		n = n * -1;
 	}
-	UM = n / 1000000;
-	cm = (n - (UM * 1000000)) / 100000;
-	dm = (n - (UM * 1000000) - (cm * 100000)) / 10000;
-	um = (n - (UM * 1000000) - (cm * 100000) - (dm * 10000)) / 1000;
-	c = (n - (UM * 1000000) - (cm * 100000) - (dm * 10000) - (um * 1000)) / 100;
-	d = (n - (UM * 1000000) - (cm * 100000) - (dm * 10000) - (um * 1000) - (c * 100)) / 10;
+	cm = n / 100000;
+	dm = (n - (cm * 100000)) / 10000;
+	um = (n - (cm * 100000) - (dm * 10000)) / 1000;
+	c = (n - (cm * 100000) - (dm * 10000) - (um * 1000)) / 100;
+	d = (n - (cm * 100000) - (dm * 10000) - (um * 1000) - (c * 100)) / 10;
 	u = n % 10;
 
-	if (UM > 0)
-		_putchar('0' + UM);
-	if (UM > 0 || cm > 0)
+	if (cm > 0)
 		_putchar('0' + cm);
-	if (UM > 0 || cm > 0 || dm > 0)
+	if (cm > 0 || dm > 0)
 		_putchar('0' + dm);
-	if (UM > 0 || cm > 0 || dm > 0 || um > 0)
+	if (cm > 0 || dm > 0 || um > 0)
 		_putchar('0' + um);
-	if (UM > 0 || cm > 0 || dm > 0 || um > 0 || c > 0)
+	if (cm > 0 || dm > 0 || um > 0 || c > 0)
 		_putchar(c + '0');
-	if (UM > 0 || cm > 0 || dm > 0 || um > 0 || c > 0 || d > 0)
+	if (cm > 0 || dm > 0 || um > 0 || c > 0 || d > 0)
 		_putchar(d + '0');
 	_putchar(u + '0');
 }
