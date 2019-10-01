c7fc1e6b3e60eb321a5d33b357250a7a84170bfc
Jorge Salazar <jormao@gmail.com>
2019-10-01 14:51:30 +0000

    I can only go one way v.0.0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_rev - function that prints a string in reverse
+ *@s: first value to check
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	for (j = i; j >=0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,4 +3,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
-void puts_half(char *str);
+void print_rev(char *s);
