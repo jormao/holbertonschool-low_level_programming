ec8285e04070ad3ebe1f58b81dfc05fd81af2b21
jormao <jormao@gmail.com>
2019-10-17 12:57:17 -0500

    0. Trust no one v0.5

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: value 1
+ *
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	unsigned int *point;
+
+	point = malloc(b);
+	if (point == NULL)
+		exit(98);
+	return (point);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c~
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: value 1
+ *
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *point;
+
+	point = malloc(b);
+	if (point == NULL)
+		exit(98);
+	return (point);
+}
