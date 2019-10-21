66e35cf39b433660a8aee3e54c416ad390bd4ea9
jormao <jormao@gmail.com>
2019-10-21 14:36:50 -0500

    Django v0.0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - define a new structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Strcture with info about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
