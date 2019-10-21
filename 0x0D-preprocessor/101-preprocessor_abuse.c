2071d3156e785b035d6eb3cec6e662d5580f001a
jormao <jormao@gmail.com>
2019-10-21 14:07:48 -0500

    eliminate all trash

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+# define x = ("Hello, Holberton")
+/**
+ * main - Print file.
+ * @void: empty
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%s\n", x);
+	return (0);
+}
