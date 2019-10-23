601b591aa8a4214ca096e34e2d19e11eca34b1c4
jormao <jormao@gmail.com>
2019-10-23 12:02:58 -0500

    functions pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - function that print a name
+ * @name: first value
+ * @f: function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - function that execute a function given an array
+ * @array: first value
+ * @size: function
+ * @action: function
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
