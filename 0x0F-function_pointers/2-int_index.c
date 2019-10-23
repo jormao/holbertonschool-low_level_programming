4c23eb903df2151ec9c595fd918fcb290de8ad93
jormao <jormao@gmail.com>
2019-10-23 13:35:11 -0500

    2. To hell with circumstances v.2.0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,19 +1,25 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * array_iterator - function that execute a function given an array
+ * int_index - function that searches an integer
  * @array: first value
  * @size: function
- * @action: function
+ * @cmp: function
  *
+ * Return: Value
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int value = 0, i;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		action(array[i]);
+		value = cmp(array[i]);
+		if (value == 1)
+			return (i);
 	}
+	return (-1);
 }
