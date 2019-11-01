0b9c3595edde7d0efc9103cdd36460d051397e34
jormao <jormao@gmail.com>
2019-11-01 04:18:27 +0000

    task 2 v.2.3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,17 +14,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *str_2;
 	int count = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-	str_2 = strdup(str);
-	new_node->str = str_2;
+	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
-	while (str_2[count])
+	while (str[count])
 		count++;
 	new_node->len = count;
 	return (new_node);
