6a4ebdacb46494a932d2d8a53dde45c21bda8fc7
jormao <jormao@gmail.com>
2019-11-01 05:10:48 +0000

    task 100 v.100.0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myStartupFun - print before main
+ *
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	print("I bore my house upon my back!\n");
+}
