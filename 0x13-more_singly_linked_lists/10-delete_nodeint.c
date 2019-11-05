24331d1289703828dd026b69994ce98f10e888cf
jormao <jormao@gmail.com>
2019-11-05 05:01:19 +0000

    task 10 v.10.0

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,15 +16,23 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *eraser;
 
 	tmp = *head;
+	if (tmp == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
 	while (count != index)
 	{
-		if (tmp == NULL)
+		if (tmp == NULL || tmp->next == NULL)
 			return (-1);
 		tmp = tmp->next;
 		count++;
 	}
-	eraser = tmp->next;
-	tmp = eraser->next;
-	free(eraser);
+	eraser = tmp->next->next;
+	free(tmp->next);
+	tmp->next = eraser;
 	return (1);
 }
