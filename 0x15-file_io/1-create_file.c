fbc7478262bbfb926188c1f1eea594ca31266b10
jormao <jormao@gmail.com>
2019-11-14 04:25:56 +0000

    change the solution for text_content is NULL, now is comillas

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		text_content = "";
 	while (text_content[count] != '\0')
 		count++;
 
