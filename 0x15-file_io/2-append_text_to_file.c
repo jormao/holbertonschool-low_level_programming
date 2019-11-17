0dc09ed7f39badacc69eea6e7853db70b95b02a1
jormao <jormao@gmail.com>
2019-11-17 04:36:39 +0000

    fix problems with point 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,6 +22,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	while (text_content[count] != '\0')
 	{
 		count++;
