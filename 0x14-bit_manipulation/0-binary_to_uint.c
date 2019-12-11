b60358d9168fdc46f25a4ea886b433510d25bf3c
jormao <55110375+jormao@users.noreply.github.com>
2019-12-10 23:41:45 -0500

    task 0 bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert from binary to unsigned int
+ * @b: binary
+ * 
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, count = 0, mult = 1;
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if ((b[i] - '0') != 0 && (b[i] - '0') != 1)
+			return (0);
+		count ++;
+		i++;
+	}
+	for (i = count - 1; i >= 0; i --)
+	{
+		if (b[i] == '1')
+			decimal += mult;
+		mult = mult * 2;
+	}
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+unsigned int binary_to_uint(const char *b);
+#endif /* _HOLBERTON */
