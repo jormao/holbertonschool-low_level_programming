0c3cc94f09597f64d6b5d2ac0a28b2d88677e533
jormao <jormao@gmail.com>
2019-12-15 01:15:11 +0000

    fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next = tmp;
 		new_node->n = n;
 		new_node->prev = NULL;
+		if (tmp != NULL)
 		*h = new_node;
 		return (new_node);
 	}
