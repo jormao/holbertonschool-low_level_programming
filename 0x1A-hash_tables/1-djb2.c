cf7ce10c490b7069423d9712fb380e88762fe427
jormao <55110375+jormao@users.noreply.github.com>
2020-01-19 02:10:48 -0500

    task1 v1.1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 
-/*
+/**
  * hash_djb2 - hash function implementing the djb2 algorithm.
  * @str: pointer to string with data
  *
@@ -10,13 +10,13 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
