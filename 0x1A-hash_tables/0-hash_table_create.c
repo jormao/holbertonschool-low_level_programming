f572b460215cf06334f62e08f2b5c2651d07bd18
jormao <jormao@gmail.com>
2020-01-19 06:51:01 +0000

    task 0 v.0.1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	
+	hash_table_t *hashtable = NULL;
+	unsigned long int i;
 
+	if (size <= 0)
+		return NULL;
+	
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return NULL;
+	
+	hashtable->size = size;
+
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+		if (hashtable->array == NULL)
+			return NULL;
+	
+	for (i = 0; i < size; i++)
+		hashtable->array[i] = NULL;
+
+	return (hashtable);
 }
